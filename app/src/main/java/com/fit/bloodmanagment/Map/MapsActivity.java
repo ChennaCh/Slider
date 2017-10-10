930d32391c2ad3ffe2b054e40c2c741412876207
ChennaCh <chennarao07@gmail.com>
2017-10-10 20:21:57 +0530

    Merge branch 'master' of https://github.com/ChennaCh/Slider
    # Conflicts:
    #	app/src/main/java/com/fit/bloodmanagment/Map/MapsActivity.java
    #	app/src/main/java/com/fit/bloodmanagment/UserProfile/SiginInActivity.java
    #	app/src/main/res/layout/activity_sigin_in.xml
    #	app/src/main/res/layout/activity_sign_up.xml

--- app/src/main/java/com/fit/bloodmanagment/Map/MapsActivity.java
+++ app/src/main/java/com/fit/bloodmanagment/Map/MapsActivity.java
@@ -480,13 +480,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
-        markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-        mCurrLocationMarker = mMap.addMarker(markerOptions);
+//        markerOptions.title("Current Position");
+//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+//        mCurrLocationMarker = mMap.addMarker(markerOptions);
 
         //move map camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
         Toast.makeText(MapsActivity.this,"Your Current Location", Toast.LENGTH_LONG).show();
 
         Log.d("onLocationChanged", String.format("latitude:%.3f longitude:%.3f",latitude,longitude));
