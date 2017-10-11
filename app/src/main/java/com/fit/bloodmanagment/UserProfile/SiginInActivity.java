bb4fbc3f66bd643546583c4c4f40dd1e0180df83
HOME <gayathriyarra488@gmail.com>
2017-10-11 22:04:14 +0530

    changes in signin and signup activities

--- app/src/main/java/com/fit/bloodmanagment/UserProfile/SiginInActivity.java
+++ app/src/main/java/com/fit/bloodmanagment/UserProfile/SiginInActivity.java
@@ -1,52 +1,25 @@
 package com.fit.bloodmanagment.UserProfile;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
-import com.fit.bloodmanagment.Activity.MyProfileActivity;
+import com.fit.bloodmanagment.Map.MapsActivity;
 import com.fit.bloodmanagment.R;
-import com.fit.bloodmanagment.Utils.CustomVolleyRequest;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-public class SiginInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+public class SiginInActivity extends AppCompatActivity {
     Toolbar toolbar;
-    //Signin button
-    private SignInButton signInButton;
-
-    //Signing Options
-    private GoogleSignInOptions gso;
-
-    //google api client
-    private GoogleApiClient mGoogleApiClient;
-
-    //Signin constant to check the activity result
-    private int RC_SIGN_IN = 100;
-
-    //TextViews
-    private TextView textViewName;
-    private TextView textViewEmail;
-    private NetworkImageView profilePhoto;
-
-    //Image Loader
-    private ImageLoader imageLoader;
-    GoogleSignInAccount acct;
-
+    EditText edtuser, edtpass;
+    String username, password;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,104 +30,38 @@ public class SiginInActivity extends AppCompatActivity implements View.OnClickLi
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
-        //Initializing google signin option
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
 
-        //Initializing signinbutton
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-        signInButton.setScopes(gso.getScopeArray());
-
-        //Initializing google api client
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-
-        //Setting onclick listener to signing button
-        signInButton.setOnClickListener(this);
-
-
-
-
+        edtuser = (EditText) findViewById(R.id.editusername);
+        edtpass = (EditText) findViewById(R.id.editpassword);
+        login=(Button)findViewById(R.id.btn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = edtuser.getText().toString();
+                password = edtpass.getText().toString();
+                if (username.equals("")) {
+//                    Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG).show();
+                    edtuser.setError("Invalid Name");
+                } else if (!isValidPassword(password)){
+                    edtpass.setError("Invalid password");}
+                else{
+                    Intent it=new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(it);
+                }
+            }
+        });
 
     }
+    private boolean isValidPassword(String Password) {
+        String Password_pattren = "^+[0-9]{6}$";
+        Pattern pattern = Pattern.compile(Password_pattren);
+        Matcher matcher = pattern.matcher(Password);
+        return matcher.matches();
 
-    //This function will option signing intent
-    private void signIn() {
-        //Creating an intent
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-
-        //Starting intent for result
-        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //If signin
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            //Calling a new function to handle signin
-            handleSignInResult(result);
-        }
-    }
-
-    //After the signing we are calling this function
-    private void handleSignInResult(GoogleSignInResult result) {
-        //If the login succeed
-        if (result.isSuccess()) {
-            //Getting google account
-            acct = result.getSignInAccount();
-             Intent intent=new Intent(SiginInActivity.this, MyProfileActivity.class);
-            intent.putExtra("name",acct.getDisplayName());
-            intent.putExtra("email",acct.getEmail());
-             intent.putExtra("image",acct.getPhotoUrl().toString());
-
-            startActivity(intent);
-            //Displaying name and email
-            textViewName.setText(acct.getDisplayName());
-            textViewEmail.setText(acct.getEmail());
-
-            //Initializing image loader
-            imageLoader = CustomVolleyRequest.getInstance(this.getApplicationContext())
-                    .getImageLoader();
-
-            imageLoader.get(acct.getPhotoUrl().toString(),
-                    ImageLoader.getImageListener(profilePhoto,
-                            R.mipmap.ic_launcher,
-                            R.mipmap.ic_launcher));
-
-            //Loading image
-            profilePhoto.setImageUrl(acct.getPhotoUrl().toString(), imageLoader);
-
-        } else {
-            //If login fails
-            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-
-
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-        //Calling signin
-                signIn();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 }
--- app/src/main/java/com/fit/bloodmanagment/UserProfile/SignUpActivity.java
+++ app/src/main/java/com/fit/bloodmanagment/UserProfile/SignUpActivity.java
@@ -1,43 +1,50 @@
 package com.fit.bloodmanagment.UserProfile;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.fit.bloodmanagment.Map.MapsActivity;
 import com.fit.bloodmanagment.R;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity  {
     Toolbar toolbar;
-
+    EditText  etname, etemail,etpassword,etre_password,etphone,etaddress,DOB;
+    Button register;
+    String name,email,password,repassward,phone,address,dateofbirth;
     //Signin button
     private SignInButton signInButton;
-
     //Signing Options
     private GoogleSignInOptions gso;
-
     //google api client
     private GoogleApiClient mGoogleApiClient;
-
     //Signin constant to check the activity result
     private int RC_SIGN_IN = 100;
-
     //TextViews
     private TextView textViewName;
     private TextView textViewEmail;
     private NetworkImageView profilePhoto;
-
     //Image Loader
     private ImageLoader imageLoader;
-
+    Calendar myCalendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,22 +55,99 @@ public class SignUpActivity extends AppCompatActivity  {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setTitle(getString(R.string.Signup));
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        etname = (EditText) findViewById(R.id.editname);
+        etemail = (EditText) findViewById(R.id.editemail);
+        etpassword = (EditText) findViewById(R.id.editpwd);
+        etre_password = (EditText) findViewById(R.id.editrepwd);
+        etphone = (EditText) findViewById(R.id.editphone);
+        etaddress = (EditText) findViewById(R.id.editaddress);
+        DOB = (EditText) findViewById(R.id.editText);
 
-        //Initializing google signin option
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-//        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-//                .enableAutoManage(getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
-//                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-//                .build();
-        //Initializing signinbutton
-        ;
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+        DOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SignUpActivity.this,date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        register = (Button) findViewById(R.id.regbtn);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = etname.getText().toString();
+                email = etemail.getText().toString();
+                password = etpassword.getText().toString();
+                repassward = etre_password.getText().toString();
+                phone = etphone.getText().toString();
+                address = etaddress.getText().toString();
+                dateofbirth=DOB.getText().toString();
+                if (!isValidName(name)) {
+                    etname.setError("Enter username");
+                }else if (!isValidEmail(email)){
+                    etemail.setError("Enter valid email");
+                }else if (password.isEmpty()){
+                    etpassword.setError("Invalid password");
+                }else if (repassward.isEmpty()){
+                    etre_password.setError("Invalid reenter password");
+                }else if (!isValidMobile(phone)){
+                    etphone.setError("Invalid phone number");
+                }else if (address.isEmpty()) {
+                    etaddress.setError("Invalid address");
+                }else if (dateofbirth.isEmpty()) {
+                    DOB.setError("Invalid date of birth");
+            }else{
+                    Intent intent=new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        DOB.setText(sdf.format(myCalendar.getTime()));
+    }
+    private boolean isValidName(String name) {
+
+        name = "^[a-zA-Z\\s]{3,30}$";
+
+        Pattern pattern = Pattern.compile(name);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
 
     }
 
+    private boolean isValidMobile(String mobile) {
+        String Mobile_PATTERN = "^+[0-9]{10}$";
+        Pattern pattern = Pattern.compile(Mobile_PATTERN);
+        Matcher matcher = pattern.matcher(mobile);
+        return matcher.matches();
 
+    }
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
